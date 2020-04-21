a6a0bbf05d3fc471d8ef18bbae4e440ccad11724
Akira Hayashi <smxshxishxad@me.com>
2020-04-21 23:58:40 +0900

    :art: Make test shorter

--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -16,9 +16,5 @@ class StockTest(unittest.TestCase):
             self.assertEqual(10, goog.price)
 
     def test_negative_price_should_throw_ValueError(self):
-            goog = Stock("GOOG")
-            try:
-                goog.update(datetime(2014, 2, 13), -1)
-            except ValueError:
-                return
-            self.fail("ValueError was not raised")
+        goog = Stock("GOOG")
+        self.assertRaises(ValueError, goog.update, datetime(2014, 2, 13), -1)
