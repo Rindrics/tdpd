3a5a0d34782f382352fcdf1cee0186077f8d5cf7
Akira Hayashi <smxshxishxad@me.com>
2020-04-23 08:15:49 +0900

    :boom: Add test for AndRule

--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -45,3 +45,22 @@ class StockTrendTest(unittest.TestCase):
     def test_increasing_trend_is_true_if_price_if_price_equal(self):
         self.given_a_series_of_prices([8, 10, 10])
         self.assertFalse(self.goog.is_increasing_trend())
+
+class AndRuleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        goog = Stock("GOOG")
+        goog.update(datetime(2014, 2, 10), 8)
+        goog.update(datetime(2014, 2, 11), 10)
+        goog.update(datetime(2014, 2, 12), 12)
+        msft = Stock("MSFT")
+        msft.update(datetime(2014, 2, 10), 10)
+        msft.update(datetime(2014, 2, 11), 10)
+        msft.update(datetime(2014, 2, 12), 12)
+        redhat = Stock("RHT")
+        redhat.update(datetime(2014, 2, 10), 7)
+        cls.exchange = {"GOOG": goog, "MSFT": msft, "RHT": redhat}
+
+    def test_an_AndRule_matches_if_all_component_rules_are_true(self):
+        rule = AndRule(PriceRule("GOOG", lambda stock: stock.price > 8), PriceRule("MSFT", lambda stock: stock.price > 10))
+        self.assertTrue(rule.matches(self.exchange))
