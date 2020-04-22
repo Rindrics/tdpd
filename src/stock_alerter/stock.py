a980aef879021b326952b9e1b4295e7456bf254b
Akira Hayashi <smxshxishxad@me.com>
2020-04-22 15:05:04 +0900

    :fire: Remove duplicated line

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -4,8 +4,6 @@ class Stock:
         self.price = None
 
     def update(self, timestamp, price):
-        self.price = price
-    def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
         self.price = price
