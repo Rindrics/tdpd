448f345d8056ba3e096efc64541f2aefcbdb1917
Akira Hayashi <smxshxishxad@me.com>
2020-04-22 15:18:38 +0900

    :boom: Add trend feature

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -1,9 +1,10 @@
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price = None
+        self.price_history = []
+
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        self.price = price
+        self.price_history.append(price)
