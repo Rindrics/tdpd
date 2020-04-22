85e923735dd7d138e1a59d0204b411d566c26097
Akira Hayashi <smxshxishxad@me.com>
2020-04-22 15:19:39 +0900

    :clown_face: Pass old tests

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -3,6 +3,10 @@ class Stock:
         self.symbol = symbol
         self.price_history = []
 
+    @property
+    def price(self):
+        return self.price_history[-1] \
+        if self.price_history else None
 
     def update(self, timestamp, price):
         if price < 0:
